838b3c28d9d0c9e3511431baf06fccaac1203000
akash singh <akash0629@gmail.com>
2024-08-04 21:34:32 +0530

    pla-146: Added unit test cases

---
+++ tests/CardGame.UnitTests/Application/Features/GetCardQueryHandlerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CardGame.Application.Contracts.Persistence;
+using CardGame.Application.Features.Card.Queries;
+using CardGame.Domain.Enums;
+
+using FluentAssertions;
+
+using Moq;
+
+namespace CardGame.UnitTests.Application.Features
+{
+    public class GetCardQueryHandlerTests
+    {
+        private readonly Mock<ICardRepository> _mockCardRepository;
+        private readonly GetCardQueryHandler _handler;
+
+        public GetCardQueryHandlerTests()
+        {
+            _mockCardRepository = new Mock<ICardRepository>();
+            _handler = new GetCardQueryHandler(_mockCardRepository.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnListOfCards_WhenCardsAreAvailable()
+        {
+            // Arrange
+            var expectedCards = new List<Domain.Entities.Card>
+            {
+                new Domain.Entities.Card(CardEnum.Hearts.ToString(), "2", 2),
+                new Domain.Entities.Card(CardEnum.Diamonds.ToString(), "3", 3)
+            };
+
+            _mockCardRepository.Setup(repo => repo.GetAllCardsAsync())
+                .ReturnsAsync(expectedCards);
+
+            var query = new GetCardQuery();
+            var cancellationToken = CancellationToken.None;
+
+            // Act
+            var result = await _handler.Handle(query, cancellationToken);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(expectedCards.Count);
+            result.Should().BeEquivalentTo(expectedCards);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnEmptyList_WhenNoCardsAvailable()
+        {
+            // Arrange
+            var expectedCards = new List<Domain.Entities.Card>();
+
+            _mockCardRepository.Setup(repo => repo.GetAllCardsAsync())
+                .ReturnsAsync(expectedCards);
+
+            var query = new GetCardQuery();
+            var cancellationToken = CancellationToken.None;
+
+            // Act
+            var result = await _handler.Handle(query, cancellationToken);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+        }
+    }
+}
---
+++ tests/CardGame.UnitTests/Application/Services/DeckServiceTests.cs
@@ -0,0 +1,73 @@
+﻿using CardGame.Application.Business;
+using CardGame.Application.Contracts;
+using CardGame.Domain.Entities;
+using FluentAssertions;
+using Moq;
+
+namespace CardGame.UnitTests.Application.Services
+{
+    public class DeckServiceTests
+    {
+        private readonly Mock<ICardService> _mockCardService;
+        private readonly DeckService _deckService;
+        public DeckServiceTests()
+        {
+            _mockCardService = new Mock<ICardService>();
+            _deckService = new DeckService(_mockCardService.Object);
+        }
+
+        [Fact]
+        public async Task GetDeck_ShouldReturnExactCount()
+        {
+            // Arrange
+            var cards = new List<Card>
+            {
+                new Card("Hearts", "2", 2),
+                new Card("Diamonds", "3", 3),
+                new Card("Clubs", "4", 4),
+                new Card("Spades", "5", 5)
+            };
+
+            _mockCardService.Setup(service => service.GetCards()).ReturnsAsync(cards);
+
+            // Act
+            var result = await _deckService.GetDeck();
+
+            // Assert
+            result.Should().HaveCount(4);
+        }
+
+        [Fact]
+        public async Task DrawCard_ShouldRemoveCardFromDeck()
+        {
+            // Arrange
+            var card = new Card("Hearts", "2", 2);
+            var cards = new List<Card> { card };
+
+            _mockCardService.Setup(service => service.GetCards()).ReturnsAsync(cards);
+
+            var deck = await _deckService.GetDeck();
+
+            // Act
+            var drawnCard = await _deckService.DrawCard(deck);
+
+            // Assert
+            drawnCard.Should().Be(card);
+            deck.Should().BeEmpty(); // Ensure the deck is now empty
+        }
+
+        [Fact]
+        public async Task DrawCard_ShouldThrowExceptionWhenDeckIsEmpty()
+        {
+            // Arrange
+            var emptyDeck = new List<Card>();
+
+            // Act
+            Func<Task> act = async () => await _deckService.DrawCard(emptyDeck);
+
+            // Assert
+            await act.Should().ThrowAsync<InvalidOperationException>()
+                .WithMessage("The deck is empty.");
+        }
+    }
+}
